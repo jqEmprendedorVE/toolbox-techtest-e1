5af5ec4fdc34b082fb3d46752751685d30a5b5f5
Julio Quintana <jquintana1801@gmail.com>
2018-03-04 19:16:21 -0300

    Ajustes finales para dejar el repo limpio para ser evaluado

--- app.js
+++ app.js
@@ -2,15 +2,3 @@ const isDivisible3or5 = require('./isDivisible3or5.js')
 
 var res = isDivisible3or5.getResult([6,15,21,25,30, 45])
 console.log(res)
-
-var res = isDivisible3or5.getResult([12,'Texto',true, 45])
-console.log(res)
-
-var res = isDivisible3or5.getResult([])
-console.log(res)
-
-var res = isDivisible3or5.getResult('[6,15,21,25,30, 45]')
-console.log(res)
-
-var res = isDivisible3or5.getResult(123)
-console.log(res)
